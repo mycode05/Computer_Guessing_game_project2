aa49e1c4c83d99764f9231c99a523317053e2f72
mycode05 <162660227+mycode05@users.noreply.github.com>
2024-05-22 21:20:24 +0530

    Update computer_guessing_game.py

--- computer_guessing_game.py
+++ computer_guessing_game.py
@@ -1,5 +1,7 @@
 #Python project2 --computer guessing number
+#importing random module to generate random intergers
 import random
+#a function for compter guess
 def comp_guess(num):
     low=1
     high=num
@@ -8,6 +10,7 @@ def comp_guess(num):
         if high != low:
             rad=random.randint(low,high)
         else:
+            #here rad variable may also be equal to low i.e, rad=low
             rad=high
         feedback=input(f"Is number {rad} Enter 'H' for high,'L' for low,'C' for correct number that you have guessed: ".lower())
         if feedback=='h':
@@ -15,6 +18,6 @@ def comp_guess(num):
         elif feedback=='l':
             low=rad+1
     print(f"Congartulations, guessed right {rad}")
-
+#user input
 number=int(input("Enter the number: "))
 comp_guess(number)
